9de7013574dc817f1709f608f13ab130c4ad4499
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2021-10-21 13:58:39 +0200

    Microservices. Partly working.

---
+++ part-05/authentication/src/main/kotlin/org/startup/authentication/AuthDto.kt
@@ -0,0 +1,12 @@
+package org.startup.authentication
+
+import javax.validation.constraints.NotBlank
+
+
+class AuthDto(
+        @get:NotBlank
+        var userId : String? = null,
+
+        @get:NotBlank
+        var password: String? = null
+)
--- part-05/e2e-tests/src/test/kotlin/org/startup/e2etests/RestTestE2E.kt
+++
@@ -1,4 +0,0 @@
-package org.startup.e2etests
-
-class RestTestE2E {
-}
